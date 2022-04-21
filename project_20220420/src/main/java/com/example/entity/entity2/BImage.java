c7c822ac22cda00a1e62197cb1c2e050676bf1bf
kku-mi <tbbk1404@gmail.com>
2022-04-21 11:18:38 +0900

    1

--- project_20220420/src/main/java/com/example/entity/entity2/BImage.java
+++ project_20220420/src/main/java/com/example/entity/entity2/BImage.java
@@ -2,7 +2,12 @@ package com.example.entity.entity2;
 
 import java.util.Date;
 
+import javax.persistence.Column;
 import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.Lob;
+import javax.persistence.ManyToOne;
 
 import lombok.Data;
 
@@ -10,10 +15,12 @@ import lombok.Data;
 @Entity
 public class BImage {
   // 게시판이미지코드
+  @Id
   private String biImgcode;
   // 게시판이미지등록일
   private Date biRegdate;
   // 게시판이미지
+  @Lob
   private String biImage;
   // 게시판이미지크기
   private String biImagesize;
@@ -21,8 +28,11 @@ public class BImage {
   private String biImagetype;
   // 게시판이미지명
   private String biImagename;
-  // 글번호
-  private String bNo;
   // 클럽글번호
+  @Column(nullable = false)
   private String cbNo;
+  // 게시판
+  @ManyToOne
+  @JoinColumn(name = "b_no")
+  private Board1 board1;
 }
