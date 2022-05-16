cd77b19dc6ccefa0518385c86f77c0e5221847e6
n10531 <durlsg531@naver.com>
2022-05-16 09:14:56 +0900

    4

--- project_20220420/src/main/java/com/example/config/WebConfig.java
+++ project_20220420/src/main/java/com/example/config/WebConfig.java
@@ -1,32 +1,32 @@
-package com.example.config;
+// package com.example.config;
 
-import org.springframework.context.annotation.Configuration;
-import org.springframework.messaging.simp.config.MessageBrokerRegistry;
-import org.springframework.web.socket.config.annotation.EnableWebSocketMessageBroker;
-import org.springframework.web.socket.config.annotation.StompEndpointRegistry;
-import org.springframework.web.socket.config.annotation.WebSocketMessageBrokerConfigurer;
+// import org.springframework.context.annotation.Configuration;
+// import org.springframework.messaging.simp.config.MessageBrokerRegistry;
+// import org.springframework.web.socket.config.annotation.EnableWebSocketMessageBroker;
+// import org.springframework.web.socket.config.annotation.StompEndpointRegistry;
+// import org.springframework.web.socket.config.annotation.WebSocketMessageBrokerConfigurer;
 
-@Configuration
-@EnableWebSocketMessageBroker
-public class WebConfig implements WebSocketMessageBrokerConfigurer{
-
-    
-
-
-
-    @Override
-    public void configureMessageBroker(MessageBrokerRegistry registry) {
-        // TODO Auto-generated method stub
-        WebSocketMessageBrokerConfigurer.super.configureMessageBroker(registry);
-    }
-
-    @Override
-    public void registerStompEndpoints(StompEndpointRegistry registry) {
-        // TODO Auto-generated method stub
-        WebSocketMessageBrokerConfigurer.super.registerStompEndpoints(registry);
-    }
+// @Configuration
+// @EnableWebSocketMessageBroker
+// public class WebConfig implements WebSocketMessageBrokerConfigurer{
 
     
 
+
+
+//     @Override
+//     public void configureMessageBroker(MessageBrokerRegistry registry) {
+//         // TODO Auto-generated method stub
+//         WebSocketMessageBrokerConfigurer.super.configureMessageBroker(registry);
+//     }
+
+//     @Override
+//     public void registerStompEndpoints(StompEndpointRegistry registry) {
+//         // TODO Auto-generated method stub
+//         WebSocketMessageBrokerConfigurer.super.registerStompEndpoints(registry);
+//     }
+
     
-}
+
+    
+// }
