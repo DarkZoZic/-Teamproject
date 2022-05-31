ad72571c94d786b18019ccdbdddd4e36cc560ed6
lley01 <lle__y01@naver.com>
2022-05-31 17:32:05 +0900

    Update CBMemView.java

--- project_20220420/src/main/java/com/example/entity/entity2/CBMemView.java
+++ project_20220420/src/main/java/com/example/entity/entity2/CBMemView.java
@@ -5,11 +5,14 @@ import javax.persistence.Entity;
 import javax.persistence.Id;
 import javax.persistence.Table;
 
+import org.springframework.data.annotation.Immutable;
+
 import lombok.Data;
 
 @Data
 @Entity
 @Table(name="CBMEMVIEW")
+@Immutable
 public class CBMemView {
 	@Id
 	@Column(name="CBNO")
