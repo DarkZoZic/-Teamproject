a6915fe5ecdea013f913200317be020ec7177fc0
GGwang2 <gegus1220@naver.com>
2022-06-01 16:01:50 +0900

    Update CBoardWriteVue.vue

--- front/src/components/clubHome/CBoard/CBoardWriteVue.vue
+++ front/src/components/clubHome/CBoard/CBoardWriteVue.vue
@@ -9,7 +9,8 @@
                 <v-col sm="8">
                     <v-row dense="" style="border-bottom: 1px solid #CCC;">
                         <v-col sm="6">
-                            <h5><router-link :to="{name : 'CHomeVue', query : {cno : state.cno}}">홈</router-link> > <router-link :to="{name : 'CBoardListVue', query : {cno : state.cno}}">{{state.boardname}}</router-link> > 글쓰기</h5>
+                            <h5><router-link :to="{ name: 'CHomeVue', query: { cno: state.cno } }">홈</router-link> > 
+                            <router-link :to="{ name: 'CBoardListVue', query: { cno: state.cno } }">{{state.boardname}}</router-link> > 글쓰기</h5>
                         </v-col>                                
                     </v-row>
 
@@ -24,20 +25,6 @@
                             <v-card style="width:100%; margin: 10px; margin-top: 20px; margin-bottom: 30px;">
                                 <v-expansion-panels style="width:100%">
                                     <v-form v-model="state.valid" style="width:100%">
-                                        <!-- 작성자 -->
-                                        <!-- <v-expansion-panel class="panel">
-                                            <v-row dense style="padding:10px;">
-                                                <v-col sm="2" style="justify-content: right; display: flex; align-items: center;">
-                                                    작성자:
-                                                </v-col>
-
-                                                <v-col sm="8" style="display: flex; align-items: center;">
-                                                    {{state.writer}}
-                                                </v-col>
-
-                                                <v-col sm="2"></v-col>
-                                            </v-row>
-                                        </v-expansion-panel> -->
 
                                         <!-- 제목 -->
                                         <v-expansion-panel class="panel">
@@ -135,7 +122,7 @@ export default {
     components: { CHHeaderVue, FooterVue, ckeditor: CKEditor.component },
     setup () {
         const router = useRouter();
-        const route = useRoute();
+        const route  = useRoute();
 
         const state = reactive({
             writer     : '작성자입니다',
@@ -146,73 +133,66 @@ export default {
             imageFile  : '',
             imageUrl   : '',
             valid      : '',
-            cno : route.query.cno,
-            token : sessionStorage.getItem("TOKEN")
+            cno        : route.query.cno,
+            token      : sessionStorage.getItem("TOKEN")
         })
 
-        const onReady = ( editor ) => {
+        const onReady = (editor) => {
             console.log(editor);
-            editor.plugins.get( 'FileRepository' ).createUploadAdapter = ( loader ) => {
-                    return new UploadAdapter( loader );
+            editor.plugins.get('FileRepository').createUploadAdapter = (loader) => {
+                    return new UploadAdapter(loader);
             };
             
-            editor.editing.view.change( writer => {
-                writer.setStyle( 'height', '600px', editor.editing.view.document.getRoot() );
+            editor.editing.view.change(writer => {
+                writer.setStyle('height', '600px', editor.editing.view.document.getRoot());
             });
             console.log(editor.editing.view);
         }
         
         const handleCancel = async() => {
             if (confirm('정말 취소하시겠습니까?') == true) {
-                router.push({ name: "CBoardListVue", query : {page : 1, cno : state.cno}});
+                router.push({ name: "CBoardListVue", query : { page: 1, cno: state.cno } });
             }
         }
 
         const write = async() => {
-            const url = `/ROOT/api/clubboard/insert`;
-            const headers = {"Content-Type" : "multipart/form-data", "token" : state.token};
-            const body = new FormData();
-            body.append("cbtitle", state.title);
+            const url     = `/ROOT/api/clubboard/insert`;
+            const headers = { "Content-Type": "multipart/form-data", token: state.token };
+            const body    = new FormData();
+            body.append("cbtitle"  , state.title);
             body.append("cbcontent", state.editorData);
-            body.append("file", state.imageFile);
-            body.append("club", state.cno);
-
+            body.append("file"     , state.imageFile);
+            body.append("club"     , state.cno);
             const response = await axios.post(url, body, {headers});
             console.log(response.data);
-            if(response.data.status === 200)
-            {
+
+            if(response.data.status === 200) {
                 alert("작성되었습니다.");
-                router.push({name: "CBoardListVue", query : {page : 1, cno : state.cno}});
+                router.push({ name: "CBoardListVue", query: { page: 1, cno: state.cno } });
             }
         }
 
-        const insertFile = (e) =>
-        {
-            if(e.target.files[0])
-            {
+        const insertFile = (e) => {
+            if(e.target.files[0]) {
                 state.imageUrl = URL.createObjectURL(e.target.files[0]);
                 state.imageFile = e.target.files[0];
-            }
-            else
-            {
+            }else {
                 state.imageUrl = null;
                 state.imageFile = null;
             }
         }
 
-        const handleToken = () =>
-        {
-            if(state.token === null)
-            {
+        const handleToken = () => {
+            if(state.token === null) {
                 alert("로그인이 필요한 페이지입니다.");
-                router.push({name:'LoginVue'});
+                router.push({ name: 'LoginVue' });
             }
         }
 
-        onMounted(()=>
-        {
+        onMounted(()=> {
             handleToken();
         })
+        
         return { state, onReady, handleCancel, write, insertFile }
     },
 }
